f8494826c0ce9f29c39337ef2411273f6386b95d
aiham <aiham@AIHAMSTRIX>
2025-06-02 20:32:51 +0300

    project structures

---
+++ Data/RedisConnectorHelper.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace MoviesAPI.Data
+{
+    public class RedisConnectorHelper
+    {
+        private static readonly Lazy<ConnectionMultiplexer> lazyConnection;
+
+        static RedisConnectorHelper()
+        {
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost:6379"); // adjust as needed
+            });
+        }
+
+        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using MoviesAPI.Data;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<MoviesDatabaseSettings>(builder.Configuration.GetSection("MoviesDatabaseSettings"));
+builder.Services.AddSingleton<MoviesService>();
+var app = builder.Build();
+
+IDatabase redis = RedisConnectorHelper.Connection.GetDatabase();
+
+app.MapGet("/", async (MoviesService moviesService) => {
+    return Results.Ok();
+});
+
+app.MapGet("/api/movies", async (MoviesService moviesService) => {
+
+    Console.WriteLine("im here");
+
+    string value = redis.StringGet("movies");
+    if (string.IsNullOrEmpty(value))
+    {
+        var movies = await moviesService.Get();
+
+        redis.StringSet("movies", JsonConvert.SerializeObject(movies));
+
+        return movies.Count > 0 ? Results.Ok(movies) : Results.NoContent();
+    }
+
+    Console.WriteLine(value);
+
+    var cachedMovies = JsonConvert.DeserializeObject<List<Movie>>(value);
+    if (cachedMovies != null && cachedMovies.Count > 0)
+    {
+        return Results.Ok(cachedMovies);
+    }
+
+    return Results.NoContent();
+});
+
+
+app.MapPost("/api/movies", async (MoviesService moviesService, Movie movie) =>
+
+{
+    await moviesService.Create(movie);
+    return Results.Ok();
+});
+
+app.Run();
